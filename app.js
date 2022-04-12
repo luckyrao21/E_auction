de7f8a52640394c6e14566a4853da659d3cb6803
jayshree01 <jayu9399@gmail.com>
2022-04-12 10:53:18 +0530

    jayshree

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const bodyparser = require("body-parser");
 const mongoose = require("mongoose");
 const adminRouter = require("./routes/admin.route")
 
-const customerRouter=require("./routes/customer.seller.route")
+const customerRouter = require("./routes/customer.seller.route")
 const BuyerRouter = require("./routes/customer.buyer.route")
-const path=require("path")
+const path = require("path")
 // const sellerRouter = require('./routes/seller.route');
 const productRouter = require('./routes/product.route');
 
@@ -22,20 +22,22 @@ mongoose.connect("mongodb+srv://lucky:1234@cluster1.bvxkm.mongodb.net/E_auction?
     console.log("not connected")
 })
 
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
 
 app.use(bodyparser.json());
 
-app.use("/",adminRouter);
-app.use("/customer",customerRouter)
-app.use("/customerBuyer",BuyerRouter)
+app.use("/", adminRouter);
+app.use("/customer", customerRouter)
+app.use("/customerBuyer", BuyerRouter)
 
 // app.use("/api/seller",sellerRouter);
 
-app.use("/api/product",productRouter);
+app.use("/api/product", productRouter);
 
 app.listen(3000, () => {
-    console.log("application is runnning.......")
+    console.log("application is runnning.....")
 })
